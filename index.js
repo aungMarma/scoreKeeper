561a71a40b62f1f73b1ca16aa43749361b2f39a5
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAsMBP.home>
2018-03-25 00:30:16 -0400

    a simple app

---
+++ index.js
@@ -0,0 +1,55 @@
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// left and right player socre
+var winning = document.getElementsByTagName("h1")[0];
+var defaultHTML = winning.innerHTML;
+var defaultCSS = winning.getAttribute("style");
+var leftPlayerScore = document.getElementsByTagName("span")[0];
+var leftPlayerScoreDefaultCSS = leftPlayerScore.getAttribute("style");
+var rightPlayerScore = document.getElementsByTagName("span")[1];
+var rightPlayerScoreDefaultCSS = rightPlayerScore.getAttribute("style");
+
+// setting winning score
+var playingTo = document.getElementsByTagName("h2")[0];
+var rule = playingTo.firstChild.nextSibling;   // span
+
+// button
+var ruleSetting = document.getElementsByTagName("input")[0];
+ruleSetting.value = 5;
+ruleSetting.addEventListener("click", function(argument) {
+	rule.textContent = this.value;
+
+})
+
+// left-side player
+var leftPlayer = document.getElementsByTagName("input")[1];
+leftPlayer.addEventListener("click", function(){
+	leftPlayerScore.textContent = Number(leftPlayerScore.textContent) + 1;
+	var score = Number(leftPlayerScore.textContent);
+	if(score === Number(rule.textContent)){
+		leftPlayerScore.style.border = "3px solid red";
+	}
+})
+
+// right-side
+var rightPlayer = document.getElementsByTagName("input")[2];
+rightPlayer.addEventListener("click", function(){
+	rightPlayerScore.textContent = Number(rightPlayerScore.textContent) + 1;
+	var score = Number(rightPlayerScore.textContent);
+	if(score === Number(rule.textContent)){
+		rightPlayerScore.style.border = "3px solid red";
+	}
+})
+
+
+// reset game to five
+var reset = document.getElementsByTagName("input")[3];
+reset.addEventListener("click", function(){
+	rule.textContent = 5;
+	ruleSetting.value = 5;
+	rightPlayerScore.textContent = 0;
+	leftPlayerScore.textContent = 0;
+	rightPlayerScore.setAttribute("style", rightPlayerScoreDefaultCSS);
+	leftPlayerScore.setAttribute("style", leftPlayerScoreDefaultCSS);
+})
